8a5ad21f0787c19da6b8d7edd7028e1099153a7f
vaisaliarjun <124667710+vaisaliarjun@users.noreply.github.com>
2025-01-06 09:26:58 +0530

    index.html

--- day_8task/frontend/src/components/Navbar.jsx
+++ day_8task/frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-logo">Expense Splitter</div>
       <div className="navbar-links">
-        <Link to="/" className="navbar-item">Home</Link>
+        <Link to="/home" className="navbar-item">Home</Link>
         <Link to="/about" className="navbar-item">About</Link>
         <Link to="/gallery" className="navbar-item">Gallery</Link>
         <Link to="/contact" className="navbar-item">Contact</Link>
